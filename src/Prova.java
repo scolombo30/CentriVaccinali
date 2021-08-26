926684a4d99d0e0b6d0ea78b4aff07bf2257d535
Sebastian Colombo <scolombo30@studenti.uninsubria.it>
2021-08-26 16:13:24 +0200

    package GUI e classe Home

--- src/Prova.java
+++
@@ -1,7 +0,0 @@
-public class Prova {
-    public static void main(String[] args) {
-
-        System.out.println("BeLaNDI");
-        System.out.println("aggiunta scritta da altro profilo");
-    }
-}
