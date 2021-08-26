db31ab8b3abf7251da0aeb843c147a2ab80397cf
Sebastian Colombo <scolombo30@studenti.uninsubria.it>
2021-08-26 16:06:32 +0200

    aggiunta scritta

--- src/Prova.java
+++ src/Prova.java
@@ -1,5 +1,7 @@
 public class Prova {
     public static void main(String[] args) {
+
         System.out.println("BeLaNDI");
+        System.out.println("aggiunta scritta da altro profilo");
     }
 }
