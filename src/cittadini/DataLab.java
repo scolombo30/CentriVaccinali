4bd6a9d725118aa766d8b305ab4011ff1623735b
Davide Franco <dfranco1@studenti.uninsubria.it>
2021-11-01 20:57:08 +0100

    aggiunta stringhe doc su DataLab.java

--- src/cittadini/DataLab.java
+++ src/cittadini/DataLab.java
@@ -9,42 +9,60 @@ public class DataLab implements Serializable {
 
 private String giorno;
 private String mese;
-private  String anno;
+private String anno;
 
-    @Override
-    public String toString() {
-        return "Data:" +
-                "giorno='" + giorno + '\'' +
-                ", mese='" + mese + '\'' +
-                ", anno='" + anno + '\'';
-    }
-
+//costruttore
+    /**
+     * @param giorno
+     * @param mese
+     * @param anno
+     */
     public DataLab(String giorno, String mese, String anno) {
         this.giorno = giorno;
         this.mese = mese;
         this.anno = anno;
     }
+    /**
+     * @return Una stringa con tutte le informazioni della data
+     */
+    @Override
+    public String toString() {
+        return "Data:" + giorno + '\'' + mese + '\'' + anno + '\'';
+    }
 
+    /**
+     * @return the giorno
+     */
     public String getGiorno() {
         return giorno;
     }
-
+    /**
+     * @param giorno the giorno to set
+     */
     public void setGiorno(String giorno) {
         this.giorno = giorno;
     }
-
+    /**
+     * @return the mese
+     */
     public String getMese() {
         return mese;
     }
-
+    /**
+     * @param mese the mese to set
+     */
     public void setMese(String mese) {
         this.mese = mese;
     }
-
+    /**
+     * @return the anno
+     */
     public String getAnno() {
         return anno;
     }
-
+    /**
+     * @param anno the anno to set
+     */
     public void setAnno(String anno) {
         this.anno = anno;
     }
