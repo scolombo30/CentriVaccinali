63f8e9307fa82b4f8e62ddb8e0a3c42f2635d459
Sebastian Colombo <scolombo30@studenti.uninsubria.it>
2021-11-09 16:19:48 +0100

    modifica

--- src/utils/Registrazione.java
+++ src/utils/Registrazione.java
@@ -1,22 +1,24 @@
 package utils;
 
+import java.sql.Connection;
+
 public  class Registrazione {
 
     public Registrazione() {}
 
-    public static void registraCentroVaccinale(){
+    public static void registraCentroVaccinale(Connection conn){
         //tab CentriVaccinali
         }
 
-    public static void registraVaccinato() {
+    public static void registraVaccinato(Connection conn) {
         //tab Vaccinati_NomeCentroVaccinale
         }
 
-    public static void registraCittadino(){
+    public static void registraCittadino(Connection conn){
         //tab Cittadini_Registrati
         }
-    public static void inserisciEventiAvversi(){}
-    public static void cercaCentroVaccinale(){}
-    public static void visualizzaInfoCentroVaccinale(){}
+    public static void inserisciEventiAvversi(Connection conn){}
+    public static void cercaCentroVaccinale(Connection conn){}
+    public static void visualizzaInfoCentroVaccinale(Connection conn){}
 
 }
