9ab13aed07000dfc918e40b19492c0f3efdb3700
Davide Franco <dfranco1@studenti.uninsubria.it>
2021-11-09 16:31:39 +0100

    aggiunti campi classe Registrazione.java e commentato metodi

--- src/utils/Registrazione.java
+++ src/utils/Registrazione.java
@@ -1,24 +1,36 @@
 package utils;
 
+import centrivaccinali.CentroVaccinale;
+import cittadini.Cittadino;
+import cittadini.EventoAvverso;
+import cittadini.Vaccinato;
+
 import java.sql.Connection;
 
 public  class Registrazione {
 
     public Registrazione() {}
-
-    public static void registraCentroVaccinale(Connection conn){
+    //metodo registra centro vaccinale sul db
+    public static void registraCentroVaccinale(Connection conn, CentroVaccinale centro){
         //tab CentriVaccinali
         }
-
-    public static void registraVaccinato(Connection conn) {
+    //metodo registra vaccianto su db
+    public static void registraVaccinato(Connection conn, Vaccinato vaccinato) {
         //tab Vaccinati_NomeCentroVaccinale
         }
-
-    public static void registraCittadino(Connection conn){
+    //metodo registra cittadino su db
+    public static void registraCittadino(Connection conn, Cittadino cittadino ){
         //tab Cittadini_Registrati
         }
-    public static void inserisciEventiAvversi(Connection conn){}
-    public static void cercaCentroVaccinale(Connection conn){}
+    //login sulla piattaforma
+    public static void loginCittadino(Connection conn,String username,String password){}
+    //metodo cerca centro vaccinale x nome
+    public static void cercaCentroVaccinaleNome(Connection conn,String nome){}
+    //metodo cerca centro vaccinale x comune, tipologia
+    public static  void cercaCentroVaccinaleCoTip(Connection conn,String comune, String tipologia){}
+    //metodo inserisci evento avverso
+    public static void inserisciEventiAvversi(Connection conn, EventoAvverso evento){}
+    //metodo visualizza info
     public static void visualizzaInfoCentroVaccinale(Connection conn){}
 
 }
