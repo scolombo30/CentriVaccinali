0b4f23d9dc77f60c4cca09e12153603ffd0c33be
Davide Franco <dfranco1@studenti.uninsubria.it>
2021-11-09 16:12:36 +0100

    aggiunto scheletro classe Registrazione.java

---
+++ src/utils/Registrazione.java
@@ -0,0 +1,22 @@
+package utils;
+
+public  class Registrazione {
+
+    public Registrazione() {}
+
+    public static void registraCentroVaccinale(){
+        //tab CentriVaccinali
+        }
+
+    public static void registraVaccinato() {
+        //tab Vaccinati_NomeCentroVaccinale
+        }
+
+    public static void registraCittadino(){
+        //tab Cittadini_Registrati
+        }
+    public static void inserisciEventiAvversi(){}
+    public static void cercaCentroVaccinale(){}
+    public static void visualizzaInfoCentroVaccinale(){}
+
+}
