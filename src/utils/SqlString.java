6618b8e4b38c61970865c7e8707a86cab2563fb3
Davide Franco <dfranco1@studenti.uninsubria.it>
2021-12-20 16:03:17 +0100

    implemtenato metodi e cambiati return altri metodi in SqlString

--- src/utils/SqlString.java
+++ src/utils/SqlString.java
@@ -6,7 +6,7 @@ public class SqlString {
 
     //String query CreaTabellaCentro
     static String creaTabellaCentro(){
-       String query = "CREATE TABLE IF NOT EXISTS CentriVaccinali ("+
+        return "CREATE TABLE IF NOT EXISTS CentriVaccinali ("+
                 "Nome_Centro VARCHAR(35),"+
                 "Tipologia VARCHAR(11)," +
                 "Qualificatore VARCHAR(7),"+
@@ -16,12 +16,12 @@ public class SqlString {
                 "Provincia VARCHAR(2),"+
                 "Cap VARCHAR(5),"+
                 "PRIMARY KEY(Nome_Centro,Comune))";
-       return query;
+
 
     }
     //String query CreaTabellaVaccinato
     public static String creaTabellaVaccinato(String nomeTabella) {
-        String query="CREATE TABLE IF NOT EXISTS "+nomeTabella+"("+
+        return "CREATE TABLE IF NOT EXISTS "+nomeTabella+"("+
                 "Nome VARCHAR(20),"+
                 "Cognome VARCHAR(20),"+
                 "Codice_fiscale VARCHAR(16) UNIQUE,"+
@@ -31,11 +31,11 @@ public class SqlString {
                 "Nome_centro VARCHAR(35),"+
                 "Comune VARCHAR(35),"+
                 "foreign key(Nome_Centro,Comune) references CentriVaccinali(Nome_Centro,Comune))";
-        return query;
+
     }
     //String query CreaTabellaCittadino
     public static String creaTabellaCittadino() {
-        String query="CREATE TABLE IF NOT EXISTS Cittadini_Registrati("+
+        return"CREATE TABLE IF NOT EXISTS Cittadini_Registrati("+
                 "Id_vax NUMERIC(16) UNIQUE,"+
                 "Nome VARCHAR(20),"+
                 "Cognome VARCHAR(20),"+
@@ -45,64 +45,65 @@ public class SqlString {
                 "PRIMARY KEY(Id_vax),"+
                 "foreign key (Username,Password) references Users (Username,Password))";
 
-        return query;
+
     }
     //String query CreaTabellaUser
     public static String creaTabellaUser() {
-        String query="CREATE TABLE IF NOT EXISTS Users("+
+        return "CREATE TABLE IF NOT EXISTS Users("+
                 "Username VARCHAR(35) UNIQUE,"+
                 "Password VARCHAR(35),"+
                 "PRIMARY KEY(Username,Password))";
-        return query;
+
     }
     //String query CreaTabellaEvento
     public static String creaTabellaEvento() {
-        String query = "CREATE TABLE IF NOT EXISTS Eventi_avversi (" +
+        return "CREATE TABLE IF NOT EXISTS Eventi_avversi (" +
                 "Tipologia VARCHAR(30)," +
                 "Severità NUMERIC(1)," +
                 "Note VARCHAR(256)," +
                 "Username VARCHAR(35))";
-        return query;
+
     }
     //metodo insert centro
     public static String insertCentro(String nome,String tipologia,String qualificatore, String nomeVia, String numeroCivico, String comune, String siglaProvincia, String cap){
-        String query= "INSERT INTO CentriVaccinali VALUES ('"+nome+"','"+tipologia+"','"+qualificatore+"','"+nomeVia+"','"
+        return "INSERT INTO CentriVaccinali VALUES ('"+nome+"','"+tipologia+"','"+qualificatore+"','"+nomeVia+"','"
                 +numeroCivico+"','"+comune+"','"+siglaProvincia+"','"+cap+"')";
-        return query;
+
     }
 
     //metodo insert vaccinato
     public static String insertVaccinato(String nomeTabella, String nome, String cognome, String cod_fisc, String tipo_vaccino, long id_vaccinazione, String comune_centro, String nomecentro,  String data_query) {
-        String query= "INSERT INTO "+nomeTabella+" VALUES ('"+nome+"', '"+cognome+"', '"+cod_fisc+"', " +
+        return "INSERT INTO "+nomeTabella+" VALUES ('"+nome+"', '"+cognome+"', '"+cod_fisc+"', " +
                 "'" +data_query+"', '"+tipo_vaccino+"', '"+id_vaccinazione+"', '"+nomecentro+"','"+comune_centro+"')";
-        return query;
+
     }
     //metodo insert users
     public static String insertUsers(String username, String password) {
-        String query="INSERT INTO Users VALUES ('"+username+"', '"+password+"')";
-        return query;
+        return "INSERT INTO Users VALUES ('"+username+"', '"+password+"')";
+
     }
     //metodo insert citadino
     public static String insertCittadino(long idVaccinazione, String nome, String cognome, String codiceFiscale,String username,String password) {
-        String query="INSERT INTO Cittadini_Registrati VALUES ('"+idVaccinazione+"', '"+nome+"', '"+cognome+"', '"+codiceFiscale+"','"+username+"','"+password+"')";
-        return query;
+        return"INSERT INTO Cittadini_Registrati VALUES ('"+idVaccinazione+"', '"+nome+"', '"+cognome+"', '"+codiceFiscale+"','"+username+"','"+password+"')";
+
     }
     //metodo insert evento
     public static String insertEvento(String tipologia, short gravità, String note, String username) {
-        String query="INSERT INTO Eventi_avversi VALUES ('"+tipologia+"', '"+gravità+"', '"+note+"', '"+username+"')";
-        return query;
+        return "INSERT INTO Eventi_avversi VALUES ('"+tipologia+"', '"+gravità+"', '"+note+"', '"+username+"')";
+
     }
     //metodo select login
     public static String selectLogin(String user,String psw) {
-        String query="SELECT * FROM Users WHERE Username='"+user+"' AND Password='"+psw+"';";
-        return query;
+        return "SELECT * FROM Users WHERE Username='"+user+"' AND Password='"+psw+"';";
+
     }
     public static String selectCentroNome(String nome){
-        String query="";
-        return query;
+        return "SELECT * FROM CentriVaccinali WHERE Nome_Centro='"+nome+"';";
+
     }
     public static String selectCentroComuneTipolgia(String comune,String tipologia){
-        String query="";
-        return query;
+        return "SELECT * FROM CentriVaccinali WHERE Comune='"+comune+"' AND Tipologia='"+tipologia+"';";
+
     }
+
 }
